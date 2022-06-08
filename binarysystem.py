9c3d0aa943aca2785648b9f2ea852cacbf85e99e
Guillaume Bertrand <guillaume@jho.fr>
2022-06-08 21:34:06 +0200

    fix

--- binarysystem.py
+++ binarysystem.py
@@ -32,20 +32,34 @@ def binarySystemObservation(obj, filename, period, jd0, step=0.05):
     with open(filename, newline='\n') as f:
         for line in f:
             p = float(line.rstrip().split(' ')[1])
-            if (p>=0):
+            if(p>=0):
                 data.append(p)
         
     for i in arange(0,1,step):
         res = None
         for d in data:
-            if d > i-step and d < i+step:
+            print(d, i, d==i)
+            if(d==i):
                 res = d
+                break
 
-        if(res):
-            results.append('\U00002705 Phase = %s : ok => %s' % (round(i,2), res))
+            ip = i + step
+            im = i - step
+            if(ip>1):
+                ip=im
+                im=(i + step)%1
+            if(im<0):
+                im=ip
+                ip=(i - step)%1
+            if (d > im and d < ip):
+                res = d
+                break
+        
+        if(res != None):
+            results.append('\U00002705 Phase = %f : ok => %s' % (round(i,2), res))
         else:
             t = findNextDateByPhase(period, i, jd0, step)
-            results.append('\U0000274C Phase = %s : next date => %s' % (round(i,2), t.iso))
+            results.append('\U0000274C Phase = %f : next date => %s' % (round(i,2), t.iso))
     
     for r in results:
         logging.info(r)
@@ -58,7 +72,7 @@ def findNextDateByPhase(period, phase, jd0, step):
     while not res:
         t = t + 1*u.d 
         p =  (t.jd-jd0) % period / period
-        if(p > phase - step and p < phase + step):
+        if(p > (phase - step) and p < phase + step):
             return t
 
 if __name__ == '__main__':
@@ -69,7 +83,7 @@ if __name__ == '__main__':
 
     # Run for mizar
     filename = '/Volumes/Samsung_T5/ASTRO/Starex/mizar/time2.lst'
-    res = binarySystemObservation('mizar', filename, 20.53835, 2459720.381331, 0.03)
+    res = binarySystemObservation('mizar', filename, 20.53835, 2459720.381331, 0.045)
 
     # Run for alpha dra
     #filename = '/Volumes/Samsung_T5/ASTRO/Starex/alphadra/time2.lst'
