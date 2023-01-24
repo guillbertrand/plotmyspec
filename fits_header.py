f111929c0bbd7171ca8d10a95544e9391a8e95c4
Guillaume Bertrand <gbe.io@pm.me>
2023-01-24 16:05:40 +0100

    fix

--- fits_header.py
+++ fits_header.py
@@ -3,24 +3,25 @@ import astropy.units as u
 from astroquery.simbad import Simbad
 from astropy.coordinates import SkyCoord
 import glob, os
-os.chdir("sandbox/220905/")
-for file in glob.glob("[!_]*.fits"):
+os.chdir("D:\ASTRO\starex\prism")
+for file in glob.glob("[!_]*-*.fits"):
     with fits.open(file, mode='update') as hdul:
         hdr = hdul[0].header
-        print(file+' >> '+hdr['OBJNAME'])
+        objname, number = file.split('-')
+        print(file+' >> '+objname)
         if 'RA' in hdr and 'DEC' in hdr:
             hdr['CRVAL1'] = (hdr['RA'], 'approx coord. in RA')
             hdr['CRVAL2'] = (hdr['DEC'], 'approx coord. in DEC')
         else:
-            result_table = Simbad.query_object(hdr['OBJNAME'])
+            result_table = Simbad.query_object(objname)
             if result_table:
                 ra = result_table[0]['RA']
-                dec = result_table[0]['RA']
+                dec = result_table[0]['DEC']
                 c = SkyCoord(ra+' '+dec, unit=(u.hourangle, u.deg))
                 crval1, crval2 = c.to_string().split(' ')
                 print('Coord ok : ', crval1, crval2)
-                hdr['CRVAL1'] = (crval1, 'approx coord. in RA')
-                hdr['CRVAL2'] = (crval2, 'approx coord. in DEC')
+                hdr['CRVAL1'] = (float(crval1), 'approx coord. in RA')
+                hdr['CRVAL2'] = (float(crval2), 'approx coord. in DEC')
     print('---')
 
 
